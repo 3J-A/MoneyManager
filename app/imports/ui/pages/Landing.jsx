da4286d96af3f73ce5d28054ccb7c782f37d5bc6
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2023-06-04 20:27:49 -1000

    Update landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,57 +1,81 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Button } from 'react-bootstrap';
 import { PageIDs } from '../utilities/ids';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div id={PageIDs.landingPage}>
     <div className="landing-green-background">
-      <Container className="text-center">
-        <h1 style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }}>
-          Welcome to Bowfolios
-        </h1>
-        <h3 style={{ paddingBottom: '20px', color: 'white' }}>
-          Profiles, projects, and interest areas for the UH Community
-        </h3>
+      <Container>
+        <Row md={1} lg={2}>
+          <Col xs={6} className="my-auto">
+            <h1 style={{ color: 'white', fontSize: '36pt' }}>
+              Managing money,
+            </h1>
+            <h1 style={{ paddingBottom: '20px', color: 'white', fontSize: '36pt' }}>
+              made simple
+            </h1>
+            <h5>
+              MoneyManager makes it easy to set up a budget that automatically monitors your spending by category. Our spend trackers and alerts help keep you on track toward your financial goals.
+            </h5>
+            <Container fluid className="text-center mt-4 py-5 px-0">
+              <a href="/signup" className="rounded-pill text-decoration-none">
+                <Button variant="primary" size="lg" className="rounded-pill"><h5 style={{ padding: '10px 35px 5px 35px ' }}>Sign up now</h5></Button>
+              </a>
+            </Container>
+          </Col>
+          <Col xs={6} className="text-center my-auto px-1">
+            <Image src="/images/mobile_1.png" className="px-1" width={240} />
+          </Col>
+        </Row>
       </Container>
     </div>
     <div className="landing-white-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: '#376551' }}>Start by making your profile....</h2>
+      <Container>
         <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/home-page.png" width={500} />
+          <Col xs={6} className="text-center my-auto pb-5 px-1">
+            <Image src="/images/mobile_1.png" className="px-1" width={240} />
           </Col>
-          <Col xs={6}>
-            <Image src="/images/profiles-page.png" width={500} />
+          <Col xs={6} className="my-auto">
+            <h1 style={{ fontSize: '36pt' }}>
+              Managing money,
+            </h1>
+            <h1 style={{ paddingBottom: '20px', fontSize: '36pt' }}>
+              made simple
+            </h1>
+            <h5>
+              MoneyManager makes it easy to set up a budget that automatically monitors your spending by category. Our spend trackers and alerts help keep you on track toward your financial goals.
+            </h5>
+            <Container fluid className="text-center mt-4 py-5 px-0">
+              <a href="/signup" className="rounded-pill text-decoration-none">
+                <Button variant="primary" size="lg" className="rounded-pill"><h5 style={{ padding: '10px 35px 5px 35px ' }}>Sign up now</h5></Button>
+              </a>
+            </Container>
           </Col>
         </Row>
       </Container>
     </div>
     <div className="landing-green-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: 'white' }}>...then add your projects</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/add-project-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/projects-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-white-background text-center">
-      <h2 style={{ color: '#376551' }}>
-        Connect to people and projects with shared interests!
-      </h2>
       <Container>
         <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/interests-page.png" width={500} />
+          <Col xs={6} className="my-auto">
+            <h1 style={{ color: 'white', fontSize: '36pt' }}>
+              Managing money,
+            </h1>
+            <h1 style={{ paddingBottom: '20px', color: 'white', fontSize: '36pt' }}>
+              made simple
+            </h1>
+            <h5>
+              MoneyManager makes it easy to set up a budget that automatically monitors your spending by category. Our spend trackers and alerts help keep you on track toward your financial goals.
+            </h5>
+            <Container fluid className="text-center mt-4 py-5 px-0">
+              <a href="/signup" className="rounded-pill text-decoration-none">
+                <Button variant="primary" size="lg" className="rounded-pill"><h5 style={{ padding: '10px 35px 5px 35px ' }}>Sign up now</h5></Button>
+              </a>
+            </Container>
           </Col>
-          <Col xs={6}>
-            <Image src="/images/filter-page.png" width={500} />
+          <Col xs={6} className="text-center my-auto px-1">
+            <Image src="/images/mobile_1.png" className="px-1" width={240} />
           </Col>
         </Row>
       </Container>
